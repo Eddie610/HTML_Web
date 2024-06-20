da8138013cfe758be2ebd8d09b6fd2e22288669d
Eddie610 <luwambo.edward@gmail.com>
2024-06-20 16:24:32 +0300

    20-06-2024

---
+++ Test_project_html/Todo.js
@@ -0,0 +1,19 @@
+var recive_text;
+var recive_date;
+if(){
+
+}
+
+ 
+      function fun() { 
+         document.getElementById("result").innerHTML = "The function fun() is triggered !"; } 
+   </script>
+   Calling js function using HTML button <br><br> 
+   <form> 
+      <label> Name : </label> 
+      <input type = "text"> </input><br><br> 
+      <input type = "button" onclick = "fun()" value = "submit"> 
+   </form> 
+   <p> 
+      <div id = "result"> </div> 
+   </p> 
